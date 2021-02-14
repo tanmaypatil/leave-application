da49b4a51715ca3fdd7cb75245a37bd78614bd79
tanmaypatil <tany.patil77@gmail.com>
2021-02-14 16:40:35 +0530

    changed the postgres password

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: 'postgres',
-    password: 'postgres',
+    password: 'postgrespassword',
     database: 'postgres'
 });
 
