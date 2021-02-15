f8acb2981914335421fa2dbe84b059bcd98abed3
tanmaypatil <tany.patil77@gmail.com>
2021-02-15 15:13:36 +0530

    using env vars

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,9 @@
 /github/workspace/node_modules/hasura-cli/hasura version
 
 cd leave-server-app/leave-system
+#print hasura host 
+echo ${HASURA_HOST}
 #perform migrate
 echo "performing migration"
-hasura migrate apply --endpoint http://localhost:8080
+hasura migrate apply --endpoint http://${HASURA_HOST}:8080
 
