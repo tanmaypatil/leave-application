bdabf3ee06883e544d11d763a81efc0e1aea1c16
tanmaypatil <tany.patil77@gmail.com>
2021-02-15 15:46:14 +0530

    hasura metadata application

--- migrate.sh
+++ migrate.sh
@@ -8,4 +8,6 @@ echo ${HASURA_HOST}
 #perform migrate
 echo "performing migration"
 hasura migrate apply --endpoint http://${HASURA_HOST}:8080
+echo "performing metadata application"
+hasura metadata apply --endpoint http://${HASURA_HOST}:8080
 
